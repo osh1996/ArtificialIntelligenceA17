ff9b652905335c2ec7ef9946aa72cced63c03b55
Unknown <tungtruo@gmail.com>
2017-08-31 19:56:45 -0400

    Division of work

--- Project_1/file.py
+++ Project_1/file.py
@@ -8,11 +8,20 @@ for item in l:
 print q
 
 #Depth 1st Search
+#Chris
 #Breath 1st Search
+#Tung
 #Depth-limited Search Depth-limit-2
+#David
 #Iterative Deepening Search showing all iterations
+#Chris
 #Uniform Cost Search
+#Tung
 #Greedy Search
+#David
 #A*
+#Chris
 #Hill-climbing
+#David
 #Beam Search W=2
+#Tung
