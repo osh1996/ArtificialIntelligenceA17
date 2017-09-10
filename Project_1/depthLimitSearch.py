bb1ec5970634fb71208d74e7148ad84207e1e76d
Unknown <dswenarton@gmail.com>
2017-09-10 14:50:26 -0400

    finished depth limited, added utility file

--- Project_1/depthLimitSearch.py
+++ Project_1/depthLimitSearch.py
@@ -1,2 +1,31 @@
+from node import Node
 
-def depthLimitSearch(nodeObjects, startNode, endNode):
+class depthLimitSearch:
+
+	#need to implement function for finding a node in nodeObjects given a name
+	def depthLimitSearch(self, nodeObjects, startNode, goalNode, limit):
+		self.nodeObjects = nodeObjects
+		self.startNode = startNode
+		self.goalNode = goalNode
+
+		recurseDepthLimitSearch(self.startNode, limit)
+
+
+	def recurseDepthLimitSearch(self, node, limit):
+		limitHit = false
+		if node.name == goalNode.name:
+			return node
+		else if limit == 0:
+			return "limit hit"
+		else:
+			for edge in node.edges:
+				child = edge[0]
+				result = recurseDepthLimitSearch(child, limit-1)
+				if result == "limit hit":
+					limitHit = true
+				else if result != failure:
+					return result
+			if limitHit:
+				return "limit hit"
+			else return "fail"
+
