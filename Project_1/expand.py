d84e8ed83a2dcdb3634a7e6088d372d4709746f1
Unknown <dswenarton@gmail.com>
2017-09-15 09:57:36 -0400

    hh

--- Project_1/expand.py
+++
@@ -1,7 +0,0 @@
-def expand(path, visited):
-	out = list()
-	edges = path.node.edges
-	for item in edges:
-		if(item[0].name not in visited):
-			Path(item[0], path, path.pathCost + item[1])
-	return out
