662e8c3278d5bbc3bc5e155d7f01f662f02a0229
Tung Truong <tungtruo@gmail.com>
2017-09-19 12:42:53 -0400

    Project 2: Alpha Beta Pruning

---
+++ Project_2/alphabetapruning.py
@@ -0,0 +1,35 @@
+from utils import *
+
+def alphabetaprune(state, game, d=4, prune_test=None, eval_Func=None):
+
+	player=game.moveto(state)
+	
+	def maxValue(state, alpha, beta, depth):
+		if prune_test(state,depth):
+			return eval_Func(state)
+		v = -infinity
+		for (a, s) in game.successors(state):
+			v = max(alpha, minValue(s, alpha, beta, depth+1))
+			if v >= beta:
+				return v
+			alpha = max(alpha, v)
+		return v
+	
+	def minValue(state, alpha, beta, depth):
+		if prune_test(state, depth):
+			return eval_Func(state)
+		v = infinity
+		for (a, s) in game.successors(state);
+			v = min(alpha, maxValue(s, alpha, beta, depth+1))
+			if v <= alpha:
+				return v
+			beta = min(beta, v)
+		return v
+	
+	prune_test = (prune_test or
+                      (lambda state, depth: depth > d or game.terminal_state(state)))
+	eval_Func = eval_Func or (lambda state: game.utility(state,player))
+	action, state = argmx(game.successors(state),
+                              lambda ((a, s)): min_value(s, -infinity, infinity, 0))
+	return action 
+	
