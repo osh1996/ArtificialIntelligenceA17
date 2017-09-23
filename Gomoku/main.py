9fac928ff8d499bdb44f89814300da120e4da138
Chris O'Shea <osh1996@gmail.com>
2017-09-22 23:02:32 -0400

    testing of file input and output

--- Gomoku/main.py
+++ Gomoku/main.py
@@ -1,40 +1,55 @@
 import os.path
 import time
 
-
-
-
-
-
-
 def main():
      end = 1
      while(end):
         presenceGo()
         if (os.path.exists("end_game")):
             end = 0
+            break
+        print "Our Turn"
         read_move()
         #AB Pruning and MiniMax algorithms
-        move = 0
-        write_move(move)
+
+        write_move("A", 1)
+
 
 def read_move():
     file = "move_file"
     f = open(file, "r")
-    move = f.read()
+
+    move = f.read().split()
+    if(len(move) > 1):
+        column = move[1]
+        row = move[2]
+    else:
+        print "No Move in File"
+
+    #pass to move function
+    print "File Read"
+    f.close()
+
+
+def write_move(column,row):
+    file = "move_file"
+    f = open(file, 'w')
+
+    move = "Terrance " + str(column) + " " + str(row)
     print move
+    f.write(move)
 
+    print "File Written"
+    f.close()
 
-
-def write_move(move):
-    b = 1
 
 def presenceGo():
     while(True):
         if(os.path.exists("Terrance.go")):
-            print "Our Turn"
             return
         time.sleep(.05)
 
+
 if __name__ == "__main__":
     main()
+
