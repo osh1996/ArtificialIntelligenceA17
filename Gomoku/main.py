d29333d6fc264e9f98729c40bc2cafd068cf04a9
Chris O'Shea <osh1996@gmail.com>
2017-09-23 19:44:23 -0400

    fixed empty file case and added comments

--- Gomoku/main.py
+++ Gomoku/main.py
@@ -1,36 +1,42 @@
 import os.path
 import time
 
-def main():
 
-     end = 1
-     while(end):
+# while not at endgame, waits for our turn, reads opponent move, then writes a move
+def main():
+    end = 1
+    while (end):
         presenceGo()
         if (os.path.exists("end_game")):
             end = 0
+            print "Reached Endgame"
             break
         print "Our Turn"
 
-        opponent_move = read_move()
-        #AB Pruning and MiniMax algorithms
+        opponent_move = read_move() #
+
+        # AB Pruning and MiniMax algorithms
 
         write_move("A", 1)
 
 
+# reads the file move_file and returns the parses the move as a list
 def read_move():
     file = "move_file"
     f = open(file, "r")
 
     move = f.read().split()
-    else:
+    if (len(move) < 3):
         print "No Move in File"
+        return None
 
-    #add move to board
     print "File Read"
     f.close()
+    return move
 
 
-def write_move(column,row):
+# takes a columns and row and writes a move to move_file
+def write_move(column, row):
     file = "move_file"
     f = open(file, 'w')
 
@@ -42,13 +48,14 @@ def write_move(column,row):
     f.close()
 
 
+# polls for presence of our team's .go file at a rate of 50Hz
 def presenceGo():
-    while(True):
-        if(os.path.exists("Terrance.go")):
+    while (True):
+        if (os.path.exists("Terrance.go")):
             return
         time.sleep(.05)
 
 
+# MAIN CALL
 if __name__ == "__main__":
     main()
-
