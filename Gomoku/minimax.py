61ab0b0c38c27563093e732f695ea7a438441bed
Unknown <dswenarton@gmail.com>
2017-09-23 02:14:55 -0400

    oops

--- Gomoku/minimax.py
+++ Gomoku/minimax.py
@@ -2,4 +2,4 @@ class Minimax:
 
     def __init__(self):
         initGrid = [['e' for x in range(15)] for y in range(15)]
-        self.root = GameState(initGrid, -float('inf'), float('inf'), )
+        self.root = GameState(initGrid, -float('inf'), float('inf'), "t")
