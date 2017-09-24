2c02a40fc618a6e654a405f77e023e24b75004e5
Chris O'Shea <osh1996@gmail.com>
2017-09-24 00:06:34 -0400

    array stuff

--- Gomoku/gametree.py
+++ Gomoku/gametree.py
@@ -1,8 +1,9 @@
-import numpy
+import numpy as np
 from gamestate import GameState
 
 class GameTree:
 
     def __init__(self):
-        init_grid = numpy.array([['e' for x in range(15)] for y in range(15)], numpy.string_)
+        init_grid = np.full((15,15), "e")
         self.root = GameState(init_grid, -float('inf'), float('inf'), "t")
+
