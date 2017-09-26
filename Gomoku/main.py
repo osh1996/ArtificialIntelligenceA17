18352b8b22b54071d1cd7aa6c03b4141679a0073
Chris O'Shea <osh1996@gmail.com>
2017-09-26 19:03:26 -0400

    added parsing file and util functions to main

--- Gomoku/main.py
+++ Gomoku/main.py
@@ -4,20 +4,20 @@ import time
 
 # while not at endgame, waits for our turn, reads opponent move, then writes a move
 def main():
-    end = 1
+    end = True
     while (end):
         presenceGo()
         if (os.path.exists("end_game")):
-            end = 0
+            end = False
             print "Reached Endgame"
             break
         print "Our Turn"
 
-        opponent_move = read_move() #
+        opponent_move = read_move()
 
         # AB Pruning and MiniMax algorithms
 
-        write_move("A", 1)
+        write_move(1, 1)
 
 
 # reads the file move_file and returns the parses the move as a list
@@ -29,6 +29,7 @@ def read_move():
     if (len(move) < 3):
         print "No Move in File"
         return None
+    move[1] = getColIndex(move[1]) #converts to index form
 
     print "File Read"
     f.close()
@@ -40,7 +41,7 @@ def write_move(column, row):
     file = "move_file"
     f = open(file, 'w')
 
-    move = "Terrance " + str(column) + " " + str(row)
+    move = "Terrance " + getColName(column) + " " + str(row) # converts to final output
     print move
     f.write(move)
 
@@ -55,6 +56,74 @@ def presenceGo():
             break
         time.sleep(.05)
 
+#takes a column string and returns an index
+def getColIndex(string):
+    if string == 'A':
+        return 0
+    if string == 'B':
+        return 1
+    if string == 'C':
+        return 2
+    if string == 'D':
+        return 3
+    if string == 'E':
+        return 4
+    if string == 'F':
+        return 5
+    if string == 'G':
+        return 6
+    if string == 'H':
+        return 7
+    if string == 'I':
+        return 8
+    if string == 'J':
+        return 9
+    if string == 'K':
+        return 10
+    if string == 'L':
+        return 11
+    if string == 'M':
+        return 12
+    if string == 'N':
+        return 13
+    if string == 'O':
+        return 14
+    return "item not on board"
+
+
+#takes a column index and returns a string
+def getColName(col):
+    if col == 0:
+        return 'A'
+    if col == 1:
+        return 'B'
+    if col == 2:
+        return 'C'
+    if col == 3:
+        return 'D'
+    if col == 4:
+        return 'E'
+    if col == 5:
+        return 'F'
+    if col == 6:
+        return 'G'
+    if col == 7:
+        return 'H'
+    if col == 8:
+        return 'I'
+    if col == 9:
+        return 'J'
+    if col == 10:
+        return 'K'
+    if col == 11:
+        return 'L'
+    if col == 12:
+        return 'M'
+    if col == 13:
+        return 'N'
+    if col == 14:
+        return 'O'
+    return "index out of bounds"
 
 # MAIN CALL
 if __name__ == "__main__":
