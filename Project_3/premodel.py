319615edafd3a06785bb5039e9dc519552348e64
Chris O'Shea <osh1996@gmail.com>
2017-10-04 19:17:50 -0400

    gitkraken

--- Project_3/premodel.py
+++ Project_3/premodel.py
@@ -1,5 +1,6 @@
+import keras
 from keras.models import Sequential
-from keras.layers import Dense, Activation
+from keras.layers import Dense, Activation, Conv2D, MaxPooling2D, Dropout, Flatten
 from keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 import numpy as np
@@ -8,19 +9,28 @@ imageFile = "images.npy"
 labelFile = "labels.npy"
 
 #Preprocessing Data
-imageData = np.load(imageFile) #loads image files
+imageData = np.load(imageFile)
+#print(imageData.shape)
+np.reshape(imageData, (6500,784)) #reshapes the array
+#print(a.shape)
 imageLabel = np.load(labelFile) #loads label files
 
 nb_classes = 10 #goes from 0 - 9 so 10 classes total
-flatData = imageData.flatten() #flattens data into a 1D vector
-print(flatData.size)
+
 hotLabel = to_categorical(imageLabel, nb_classes) #turns labels into hotLabels
-print(hotLabel.size)
-#hotLabel = np.eye(nb_classes)[imageLabel]
 
 #Randomly splitting up data
-x, x_Test, y, y_Test = train_test_split(flatData,hotLabel,test_size = 0.25, train_size= 0.75)
+
+x, x_Test, y, y_Test = train_test_split(imageData,hotLabel,test_size = 0.25, train_size= 0.75)
 x_Train, x_Val, y_Train, y_Val = train_test_split(x,y,test_size = 0.2 ,train_size = 0.8)
+# print(x.shape)
+# print(x_Test.shape)
+# print(y.shape)
+# print(y_Test.shape)
+# print(x_Train.shape)
+# print(x_Val.shape)
+# print(y_Train.shape)
+# print(y_Val.shape)
 
 
 # Model Template
@@ -28,15 +38,9 @@ x_Train, x_Val, y_Train, y_Val = train_test_split(x,y,test_size = 0.2 ,train_siz
 model = Sequential() # declare model
 model.add(Dense(10, input_shape=(28*28, ), kernel_initializer='he_normal')) # first layer
 model.add(Activation('relu'))
-#
-#
-#
-# Fill in Model Here
-#
-#
+
 model.add(Dense(10, kernel_initializer='he_normal')) # last layer
 model.add(Activation('softmax'))
-
 
 # Compile Model
 model.compile(optimizer='sgd',
@@ -48,10 +52,10 @@ history = model.fit(x_Train, y_Train,
                     validation_data = (x_Val, y_Val),
                     epochs=10,
                     batch_size=512,
-                    verbose=1) 
+                    verbose=1)
 
 
 # Report Results
 
 print(history.history)
-model.predict()
+#model.predict()
